ccfff6e39c40679853588963af06a69d6cddc3af
yurigalindo <yurioliveiragalindo@gmail.com>
2024-03-22 14:54:46 -0300

    feat: now records games

---
+++ source/LLM_utils.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from os import listdir
+
+def colors_to_prompt(guess,colors):
+    prompts = []
+    for letter,color in zip(guess,colors):
+        prompt = f"{letter} - "
+        if color == "green":
+            prompt += "This letter is in the correct spot."
+        elif color == "yellow":
+            prompt += "This letter is present in the word but in another spot."
+        else:
+            prompt += "This letter is not present in the word."
+        prompts.append(prompt)
+    return "".join(prompts)
+
+class GameLogger():
+    def __init__(self):
+        self.logs = []
+        self.dir = "data/game_examples/"
+    def log_guess(self,guess):
+        self.logs.append(("guess",guess))
+    def log_feedback(self,feedback):
+        self.logs.append(("feedback",feedback))
+    def compile_logs(self):
+        df = pd.DataFrame(self.logs)
+        df.to_csv(self.dir + f"{len(listdir(self.dir))}.csv")
--- source/main.py
+++ source/main.py
@@ -1,22 +1,35 @@
 from wordle import get_word, check_answer
 from termcolor import colored
+from LLM_utils import GameLogger, colors_to_prompt
 
 
 def main():
   word = get_word()
+  logger = GameLogger()
+  logger.log_feedback("Write a 5-letter word to start the game:")
+
   for _ in range(6):
+    # Ask for guess
     guess = ""
     while len(guess)!=5:
-      guess = input("Write a 5-letter word:")
+      guess = input("Write a 5-letter word to start the game:")
     guess = guess.upper()
+    logger.log_guess(guess)
+
+    # Check guess against word
     colors = check_answer(guess,word)
+    logger.log_feedback(colors_to_prompt(guess,colors))
     text = [colored(guess[i],colors[i]) for i in range(5)]
     text = "".join(text)
     print(text)
     if guess.upper() == word:
        print("You have correctly guessed the word, congratulations!")
+       logger.log_feedback("You have correctly guessed the word, congratulations!")
+       logger.compile_logs()
        return
   print(f"You have exhausted your guesses. The correct word was: {word}")
+  logger.log_feedback(f"You have exhausted your guesses. The correct word was: {word}")
+  logger.compile_logs()
 
 if __name__ == '__main__':
     main()
